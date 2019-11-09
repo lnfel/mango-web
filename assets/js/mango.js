6eb6059681f5e53fa44f2292380dc821823ac293
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2019-11-09 18:28:44 +0800

    Changed trianglify color scheme

--- assets/js/mango.js
+++ assets/js/mango.js
@@ -17,7 +17,7 @@ var rs = Math.floor((Math.random() * 11) + 4);
 });
 var pattern = t.generate(window.innerWidth, window.innerWidth+200);
 //document.body.setAttribute('style', 'background-image: '+pattern.dataUrl);
-document.getElementsByTagName("header")[0].setAttribute('style', 'background-image: '+pattern.dataUrl);
+//document.getElementsByTagName("header")[0].setAttribute('style', 'background-image: '+pattern.dataUrl);
 
 $(document).ready(function(){
   $('ul.tabs').tabs({
